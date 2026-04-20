ad9fbf621b99099a52defd7cfaa836a18de39741
Anthony <anthony@example.com>
2026-04-20 12:58:17 -0400

    chore: GitHub Actions deploy + SPA 404 redirect

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,13 @@ import App from './App.tsx';
 
 const basename = import.meta.env.BASE_URL;
 
+// Handle 404 redirect
+const params = new URLSearchParams(location.search);
+const redirect = params.get('redirect');
+if (redirect) {
+  history.replaceState(null, '', import.meta.env.BASE_URL + redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
